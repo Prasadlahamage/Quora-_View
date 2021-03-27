9360df09e96eb8d3bc61bfa13a2e2c5d34e5abcf
Prasad lahamage <52752672+Prasadlahamage@users.noreply.github.com>
2021-03-27 14:12:14 +0530

    Update Proxy.py

--- Proxy.py
+++ Proxy.py
@@ -1,9 +1,6 @@
 import requests
 from lxml.html import fromstring
 from itertools import cycle
-import os
-
-from selenium import webdriver
 
 def get_proxies():
     URL = 'https://free-proxy-list.net/'
@@ -28,7 +25,7 @@ def proxies(URL, No_of_IP):
         print("Request #%d" % i)
 
         try:
-            response = requests.get(url, proxies={"http": proxy, "https": proxy})
+            response = requests.get(url, proxies={"http": proxy, "https": proxy}, timeout=15)
             result = response.json()
         except:
             # Most free proxies will often get connection errors.
@@ -40,23 +37,10 @@ def proxies(URL, No_of_IP):
 
         if result != "Skipping, Connection error":
             j += 1
-            chrome_options=webdriver.ChromeOptions()
-            chrome_options.binary_location=os.environ.get("GOOGLE_CHROME_BIN")
-            chrome_options.add_argument("--headless")
-            chrome_options.add_argument("--disable-dev-shm-usage")
-            chrome_options.add_argument("--no-sandbox")
-            webdriver.DesiredCapabilities.CHROME['proxy'] = {"httpProxy": proxy, "ftpProxy": proxy, "sslProxy": proxy,
-                                                            "proxyType": "MANUAL", }
-            driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
             try:
-                # time.sleep(5)
-                driver.get(url=URL)
-                # time.sleep(5)
-                # contents = driver.find_element_by_partial_link_text("what is my ip")
-                # contents.click()
+                requests.get(URL, proxies={"http": proxy, "https": proxy}, timeout=20)
             except:
                 print("Not")
-            driver.close()
     j = str(j)
 
     return "<h2>IP's hitting on URL: %s"%(j)+"</h2>"
